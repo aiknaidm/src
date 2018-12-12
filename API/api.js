bbcfedeaebc17ae5fe99ebc09224d15a9d63e916
salad1987 <qianxunde1987@163.com>
2018-12-12 14:25:20 +0800

    易站名及结束时间 提到外层

--- API/api.js
+++ API/api.js
@@ -1,14 +1,15 @@
-const host = "https://lmbge.com/wxapi"
+const host = "https://lmbge.com/wxapi/pifaban"
+const host_test = "https://lmbge.com/wxapi"
 const api = {
-    login: `${host}/pifaban/denglu`, //登录
-    getUserInfo: `${host}/pifaban/get_user_info`, //用户信息
-    ad: `${host}/pifaban/guanggao`,
-    message: `${host}/pifaban/zixun`, //资讯
-    carousel: `${host}/pifaban/lunbo`, //轮播图
-    yizhanIndex: `${host}/jicai/wdyz`, //商城首页
-    yizhanBanner: `${host}/jicai/yzgg`, //首页广告
-    orderList: `${host}/jicai/orderlist?order_status=4`, //进货记录（已完成订单）
-    yizhanList: `${host}/jicai/yzlb_my`, //易站列表
+    login: `${host}/denglu`, //登录
+    getUserInfo: `${host}/get_user_info`, //用户信息
+    ad: `${host}/guanggao`,
+    message: `${host}/zixun`, //资讯
+    carousel: `${host}/lunbo`, //轮播图
+    yizhanIndex: `${host}/wdyz`, //商城首页
+    yizhanBanner: `${host}/yzgg`, //首页广告
+    orderList: `${host}/orderlist?order_status=4`, //进货记录（已完成订单）
+    yizhanList: `${host}/yzlb_my`, //易站列表
 
 }
 export default api
