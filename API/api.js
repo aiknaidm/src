a867f9ea4dc0ceba259d321f5e4d6f2c5d9292eb
aiknaidm <aiknaidm@163.com>
2018-12-14 17:43:17 +0800

    订单列表

--- API/api.js
+++ API/api.js
@@ -3,12 +3,13 @@ const host_test = "https://lmbge.com/wxapi"
 const api = {
     login: `${host}/denglu`, //登录
     getUserInfo: `${host}/get_user_info`, //用户信息
+    upUserInfo: `${host}/up_user_info`,
     ad: `${host}/guanggao`,
     message: `${host}/zixun`, //资讯
     carousel: `${host}/lunbo`, //轮播图
     yizhanIndex: `${host}/wdyz`, //商城首页
     yizhanBanner: `${host}/yzgg`, //首页广告
-    orderList: `${host}/orderlist?order_status=4`, //进货记录（已完成订单）
+    orderList: `${host}/orderlist`, //进货记录（已完成订单）
     yizhanList: `${host}/yzlb_my`, //易站列表
     goodsKind: `${host}/hyfl`, //商品分类
     goodsList: `${host}/search`, //商品列表
