6166fa74df50031f398efcc660589cf3b9c34190
salad1987 <qianxunde1987@163.com>
2019-04-02 17:17:06 +0800

    剩余api

--- API/api.js
+++ API/api.js
@@ -1,9 +1,6 @@
 const host = "https://lmbge.com/wxapi/pifaban"
 const host_test = "https://lmbge.com/wxapi"
 const api = {
-  unfriend: `${host}/jcgx`, //解除关系//取消申请
-  removeindex: `${host}/qxxb`, //取消店铺首页
-  setIndex: `${host}/swxb`, //设为店铺首页
   cardwxpay: `${host}/cardwxpay`,
   cardpaysuccess: `${host}/cardpaysuccess`,
 
