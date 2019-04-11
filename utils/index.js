232f90c268c3b39636c823821f4e4e06b9b704e9
salad1987 <qianxunde1987@163.com>
2019-04-11 16:39:48 +0800

    商品详情 回到首页

--- utils/index.js
+++ utils/index.js
@@ -130,13 +130,13 @@ const html_decode = function (str) {
   // s = str.replace(/&gt;/g, "&");
   s = str.replace(/\r/g, "");
   s = s.replace(/\n/g, "<br/>");
-  s = s.replace(/& amp; /g, "&");
-  s = s.replace(/& amp; /g, "&");
-  s = s.replace(/& lt; /g, "<");
-  s = s.replace(/& gt; /g, ">");
-  s = s.replace(/& nbsp; /g, " ");
-  s = s.replace(/& #39; /g, "\'");
-  s = s.replace(/& quot; /g, "\"");
+  s = s.replace(/&amp;/g, "&");
+  s = s.replace(/&amp;/g, "&");
+  s = s.replace(/&lt;/g, "<");
+  s = s.replace(/&gt;/g, ">");
+  s = s.replace(/&nbsp;/g, " ");
+  s = s.replace(/&#39;/g, "\'");
+  s = s.replace(/&quot;/g, "\"");
 
   return s;
 }
